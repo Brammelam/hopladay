43dc1a40af94e274bd78963620939724d6a7e425
Borm <brammelam@hotmail.no>
2025-11-07 21:54:57 +0100

    fix backend port and main

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,8 +24,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connected to MongoDB");
-    app.listen(process.env.PORT || 4000, () =>
-      console.log(`🚀 Server running on port ${process.env.PORT || 4000}`)
+    app.listen(process.env.PORT ?? 4000, () =>
+      console.log(`🚀 Server running on port ${process.env.PORT ?? 4000}`)
     );
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
